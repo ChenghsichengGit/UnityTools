9ec25c2ef1f56c3412e627a818ece83a55cb225d
cheng <kosakijp@gmail.com>
2024-03-18 04:30:25 +0800

    update: 完善判斷節點

--- Assets/NodeEditorTool/Node/JudgeNode.cs
+++ Assets/NodeEditorTool/Node/JudgeNode.cs
@@ -39,6 +39,10 @@ public class JudgeNode : DecoratorNode
         {
             return JudgeFloat();
         }
+        if (variableType == VariableType.Bool)
+        {
+            return JudgeBool();
+        }
 
         return State.Failure;
     }
@@ -114,6 +118,25 @@ public class JudgeNode : DecoratorNode
         if (judgeType == JudgeType.less)
         {
             if (a < b)
+            {
+                child.Update();
+                return State.Success;
+            }
+            else
+                return State.Failure;
+        }
+
+        return State.Failure;
+    }
+
+    private State JudgeBool()
+    {
+        bool a = variables.GetBool(variableName);
+        bool b = compareVariable == "true";
+
+        if (judgeType == JudgeType.equal)
+        {
+            if (a == b)
             {
                 child.Update();
                 return State.Success;
